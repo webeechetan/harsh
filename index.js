123bd119b6f25c546acb861908766d203d029357
Chetan Singh <chetan.singh@webeesocial.com>
2022-07-09 13:55:08 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,50 @@
+// document.write('Xyz');
+// console.log('this is log');
+// alert('harsh');
+
+
+
+////string
+// var user_details = "user name is harsh and his contact number is 73824862348346";
+
+
+///number
+var contact = 9753591245;
+
+
+////float
+var price = 99.99;
+
+///array   array always start from 0
+// var students = ["harsh", "73824862348346","1212122"];
+
+
+///object 
+var student = {roll_numer:'12121212',name:'harsh',contact:'972452535'};
+
+
+
+var students = [
+    {roll_numer:'12121212',name:'harsh',contact:'972452535'},
+    {roll_numer:'438747384',name:'rahul',contact:'453434'},
+    {roll_numer:'894834738',name:'abc',contact:'590675968596'},
+    {roll_numer:'47564587',name:'xyz',contact:'854574857'},
+    {roll_numer:'837438',name:'lakhan',contact:'84574857'}
+];
+
+
+
+console.log("this name of the student is " + students[0].name + " and his roll number is "+ students[0].roll_numer+ " and his contact number is "+students[0].contact)
+console.log("this name of the student is " + students[1].name + " and his roll number is "+ students[1].roll_numer+ " and his contact number is "+students[1].contact)
+console.log("this name of the student is " + students[2].name + " and his roll number is "+ students[2].roll_numer+ " and his contact number is "+students[2].contact)
+console.log("this name of the student is " + students[3].name + " and his roll number is "+ students[3].roll_numer+ " and his contact number is "+students[3].contact)
+console.log("this name of the student is " + students[4].name + " and his roll number is "+ students[4].roll_numer+ " and his contact number is "+students[4].contact)
+
+
+
+
+
+// console.log(student.contact);
+
+
+// alert(contact)
